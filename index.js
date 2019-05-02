59dc89249a8e5faf0f40574faac84a4d7e2f564a
jwy5140 <jwy5140@gmail.com>
2019-05-02 00:40:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(str){
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+  static sanitize(str){
+    return str.replace(/[^\w\s'-]/g,"")
+  }
+  static titleize(str){
+    let arr = str.split(" ")    
+    let noCaps = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    arr.forEach(function (word,n){
+      if (n === 0 || !noCaps.includes(word))
+      {
+        arr[n] = word.charAt(0).toUpperCase() + word.slice(1)
+      }
+    }
+    )
+    return arr.join(" ")
+  }
 }
